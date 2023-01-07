407cace4f90ef3d4e3607f5f542268fd554b09cd
Igor Zateryukin <igor.zateryukin@rt.ru>
2023-01-07 17:19:02 +0300

    Move to separate file

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from unused_attrs import decorate_all_classes
+
 
 class C:
     def __init__(self):
@@ -5,46 +7,10 @@ class C:
         self.b = 1
 
 
-def for_all_attrs(original_class):
-    orig_init = original_class.__init__
-    orig_getattr = original_class.__getattribute__
-    if "__read_count" not in globals():
-        global __read_count
-        __read_count = dict()
-    if original_class.__name__ not in __read_count:
-        __read_count[original_class.__name__] = dict()
-
-    # Make copy of original __init__, so we can call it without recursion
-
-    def __init__(self, *args, **kws):
-        orig_init(self, *args, **kws) # Call the original __init__
-        global __read_count
-        for attr_name in dir(self):
-            if not attr_name.endswith('__') and not callable(getattr(self, attr_name)):
-                __read_count[original_class.__name__][attr_name] = 0
-
-    def __getattribute__(self, attr_name):
-        global __read_count
-        if attr_name in __read_count[original_class.__name__]:
-            __read_count[original_class.__name__][attr_name] += 1
-        return orig_getattr(self, attr_name)
-
-    original_class.__init__ = __init__ # Set the class' __init__ to the new one
-    original_class.__getattribute__ = __getattribute__
-    return original_class
-
-import inspect
-for obj in [obj for obj in vars().values() if inspect.isclass(obj)]:
-        obj = for_all_attrs(obj)
+decorate_all_classes(vars())
 
 c = C()
 c.a = 2
 c.b = 3
 print(c.a)
 
-
-if "__read_count" in globals():
-    for class_name, attrs in __read_count.items():
-        for attr, count in attrs.items():
-            if count == 0:
-                print(f"Warning in class {class_name} variable {attr} is never read")
---
+++ unused_attrs.py
@@ -0,0 +1,45 @@
+import atexit
+
+
+def for_all_attrs(original_class):
+    orig_init = original_class.__init__
+    orig_getattr = original_class.__getattribute__
+    if "__read_count" not in globals():
+        global __read_count
+        __read_count = dict()
+    if original_class.__name__ not in __read_count:
+        __read_count[original_class.__name__] = dict()
+
+    # Make copy of original __init__, so we can call it without recursion
+
+    def __init__(self, *args, **kws):
+        orig_init(self, *args, **kws) # Call the original __init__
+        global __read_count
+        for attr_name in dir(self):
+            if not attr_name.endswith('__') and not callable(getattr(self, attr_name)):
+                __read_count[original_class.__name__][attr_name] = 0
+
+    def __getattribute__(self, attr_name):
+        global __read_count
+        if attr_name in __read_count[original_class.__name__]:
+            __read_count[original_class.__name__][attr_name] += 1
+        return orig_getattr(self, attr_name)
+
+    original_class.__init__ = __init__ # Set the class' __init__ to the new one
+    original_class.__getattribute__ = __getattribute__
+    return original_class
+
+
+def decorate_all_classes(vars):
+    import inspect
+    for obj in [obj for obj in vars.values() if inspect.isclass(obj)]:
+            obj = for_all_attrs(obj)
+    atexit.register(print_report)
+
+
+def print_report():
+    if "__read_count" in globals():
+        for class_name, attrs in __read_count.items():
+            for attr, count in attrs.items():
+                if count == 0:
+                    print(f"Warning in class {class_name} variable {attr} is never read")
